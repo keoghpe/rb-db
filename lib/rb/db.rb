89bde7a1bdd57d7b9da997913ef4e58f3139416b
Peter Keogh <peterkeoghdev@gmail.com>
2021-03-19 10:07:53 +0000

    Add some type checking

--- lib/rb/db.rb
+++ lib/rb/db.rb
@@ -1,3 +1,4 @@
+# typed: strict
 # frozen_string_literal: true
 
 require_relative "db/version"
@@ -5,11 +6,20 @@ require_relative "db/version"
 module Rb
   module Db
     class Database
+      extend T::Sig
+      sig {void}
       def initialize
+      end
+
+      sig {params(key: Integer, value: T::Hash[T.untyped, T.untyped]).returns(NilClass)}
+      def set(key, value)
+      end
+
+      sig {params(key: Integer).returns(NilClass)}
+      def get(key)
       end
     end
 
     class Error < StandardError; end
-    # Your code goes here...
   end
 end
--- spec/rb/db_spec.rb
+++ spec/rb/db_spec.rb
@@ -1,3 +1,4 @@
+# typed: false
 # frozen_string_literal: true
 
 RSpec.describe Rb::Db do
