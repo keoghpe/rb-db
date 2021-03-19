43e02d43ccedb7da36fca893460059b0a6be5913
Peter Keogh <peterkeoghdev@gmail.com>
2021-03-19 11:14:09 +0000

    Add get / set

--- lib/rb/db.rb
+++ lib/rb/db.rb
@@ -2,21 +2,31 @@
 # frozen_string_literal: true
 
 require_relative "db/version"
+require 'sorbet-runtime'
+require 'json'
 
 module Rb
   module Db
     class Database
+      LOG_FILE = T.let("./aof.txt", String)
+
       extend T::Sig
       sig {void}
       def initialize
+        @log = T.let(File.open(LOG_FILE, 'a+'), File)
+        @log.sync = true
       end
 
       sig {params(key: Integer, value: T::Hash[T.untyped, T.untyped]).returns(NilClass)}
       def set(key, value)
+        @log.write("#{key},#{value.to_json}\n"); nil
       end
 
-      sig {params(key: Integer).returns(NilClass)}
+      sig {params(key: Integer).returns(T::Hash[T.untyped, T.untyped])}
       def get(key)
+        File.open(LOG_FILE, 'r')
+            .each_line.grep(/^#{key},/).last
+            &.gsub(/^\d+,/, '')&.yield_self {|h| JSON.parse(h)}
       end
     end
 
