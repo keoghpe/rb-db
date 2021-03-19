58c3839cb78d4298c9bba991cf7144d1e53ea75f
Peter Keogh <peterkeoghdev@gmail.com>
2021-03-19 12:03:48 +0000

    Populate the index on create

--- lib/rb/db.rb
+++ lib/rb/db.rb
@@ -11,14 +11,15 @@ module Rb
       LOG_FILE = T.let("./aof.txt", String)
 
       extend T::Sig
-      sig {void}
+      sig { void }
       def initialize
         @log = T.let(File.open(LOG_FILE, 'a+'), File)
         @log.sync = true
         @index = T.let({}, T::Hash[Integer, Integer])
+        populate_index
       end
 
-      sig {params(key: Integer, value: T::Hash[T.untyped, T.untyped]).returns(NilClass)}
+      sig { params(key: Integer, value: T::Hash[T.untyped, T.untyped]).returns(NilClass) }
       def set(key, value)
         line = "#{key},#{value.to_json}\n"
         @log.write(line)
@@ -28,14 +29,39 @@ module Rb
         nil
       end
 
-      sig {params(key: Integer).returns(T::Hash[T.untyped, T.untyped])}
+      sig { params(key: Integer).returns(T::Hash[T.untyped, T.untyped]) }
       def get(key)
         file = File.open(LOG_FILE, 'r')
-        file.pos = T.must(@index[key])
-        file.readline.gsub(/^\d+,/, '').yield_self {|h| JSON.parse(h)}
+        if @index[key]
+          file.pos = T.must(@index[key])
+          file.readline.gsub(/^\d+,/, '').yield_self { |h| JSON.parse(h) }
+        else
+          raise KeyNotFound
+        end
+      end
+
+      private
+
+      sig { returns(NilClass) }
+      def populate_index
+        file = File.open(LOG_FILE, 'r')
+        loop do
+          begin
+            current_pos = file.pos
+            line = file.readline
+            key = T.must(line.match(/^(\d+),/))[1].to_i
+            @index[key] = current_pos
+          rescue EOFError
+            break
+          end
+        end
       end
     end
 
-    class Error < StandardError; end
+    class KeyNotFound < StandardError
+    end
+
+    class Error < StandardError;
+    end
   end
 end
