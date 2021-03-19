f79b3dee392aad63fc551c78be8744db74a0c288
Peter Keogh <peterkeoghdev@gmail.com>
2021-03-19 11:41:38 +0000

    Basic hash index

--- lib/rb/db.rb
+++ lib/rb/db.rb
@@ -15,18 +15,24 @@ module Rb
       def initialize
         @log = T.let(File.open(LOG_FILE, 'a+'), File)
         @log.sync = true
+        @index = T.let({}, T::Hash[Integer, Integer])
       end
 
       sig {params(key: Integer, value: T::Hash[T.untyped, T.untyped]).returns(NilClass)}
       def set(key, value)
-        @log.write("#{key},#{value.to_json}\n"); nil
+        line = "#{key},#{value.to_json}\n"
+        @log.write(line)
+        # does the below work for UTF-8 chars?
+        byte_offset = @log.pos - line.length
+        @index[key] = byte_offset
+        nil
       end
 
       sig {params(key: Integer).returns(T::Hash[T.untyped, T.untyped])}
       def get(key)
-        File.open(LOG_FILE, 'r')
-            .each_line.grep(/^#{key},/).last
-            &.gsub(/^\d+,/, '')&.yield_self {|h| JSON.parse(h)}
+        file = File.open(LOG_FILE, 'r')
+        file.pos = T.must(@index[key])
+        file.readline.gsub(/^\d+,/, '').yield_self {|h| JSON.parse(h)}
       end
     end
 
